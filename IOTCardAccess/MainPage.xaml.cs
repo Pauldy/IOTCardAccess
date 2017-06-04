a95e883ca61d214607f42d4ea4e1fe657bac17a9
Paul <github@timespace.org>
2017-06-04 14:29:15 -0500

    Stubbing out the comms and display part of the app

--- IOTCardAccess/MainPage.xaml.cs
+++ IOTCardAccess/MainPage.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +27,109 @@ namespace IOTCardAccess
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SerialDevice serialPort = null;
+        DataWriter dataWriteObject = null;
+        DataReader dataReaderObject = null;
+        private CancellationTokenSource ReadCancellationTokenSource;
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            initSerial();
         }
+
+        private async void initSerial()
+        {
+            string deviceSelector = SerialDevice.GetDeviceSelector("UART0");
+            var deviceInformation = await DeviceInformation.FindAllAsync(deviceSelector, null);
+
+            if (deviceInformation.Count > 0)
+            {
+                serialPort = await SerialDevice.FromIdAsync(deviceInformation[0].Id);
+                serialPort.BaudRate = 9600;
+                serialPort.StopBits = SerialStopBitCount.One;
+                serialPort.Parity = SerialParity.None;
+                serialPort.DataBits = 8;
+                serialPort.Handshake = SerialHandshake.None;
+                ReadCancellationTokenSource = new CancellationTokenSource();
+                StartListening();
+            }
+            else
+            {
+                serialOutListView.Items.Add("Unable to initialize serial port");
+            }
+
+
+        }
+
+        private async void StartListening()
+        {
+
+            try
+            {
+                if (serialPort != null)
+                {
+                    dataReaderObject = new DataReader(serialPort.InputStream);
+
+                    // keep reading the serial input
+                    while (true)
+                    {
+                        await ReadAsync(ReadCancellationTokenSource.Token);
+                    }
+                }
+            }
+            catch (TaskCanceledException tce)
+            {
+                serialOutListView.Items.Add("Failed to cancel read:" + tce.Message);
+            }
+            catch (Exception ex)
+            {
+                serialOutListView.Items.Add("Read failed:" + ex.Message);
+            }
+            finally
+            {
+                // Cleanup once complete
+                if (dataReaderObject != null)
+                {
+                    dataReaderObject.DetachStream();
+                    dataReaderObject = null;
+                }
+            }
+        }
+
+        private string currentLine = "";
+
+        private async Task ReadAsync(CancellationToken cancellationToken)
+        {
+            Task<UInt32> loadAsyncTask;
+
+            uint ReadBufferLength = 1024;
+
+            // If task cancellation was requested, comply
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // Set InputStreamOptions to complete the asynchronous read operation when one or more bytes is available
+            dataReaderObject.InputStreamOptions = InputStreamOptions.Partial;
+
+            using (var childCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
+            {
+                // Create a task object to wait for data on the serialPort.InputStream
+                loadAsyncTask = dataReaderObject.LoadAsync(ReadBufferLength).AsTask(childCancellationTokenSource.Token);
+
+                // Launch the task and wait
+                UInt32 bytesRead = await loadAsyncTask;
+                if (bytesRead > 0)
+                {
+                    currentLine += dataReaderObject.ReadString(bytesRead);
+                    // here we process to determine if we are at the end of a read line, need to wait for more data, or reset the currentLine
+                    if(currentLine.Length>25)
+                    {
+
+                    }
+                }
+            }
+        }
+
     }
 }
